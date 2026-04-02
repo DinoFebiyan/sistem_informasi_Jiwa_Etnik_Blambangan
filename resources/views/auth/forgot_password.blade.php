a01ca366f177e975d7f114ceec6fc54d1a9afd1d
Febriyanputra23 <febriyanputra23@gmail.com>
2026-04-02 12:54:08 +0700

    lupa_password_perbaikan

--- resources/views/auth/forgot_password.blade.php
+++ resources/views/auth/forgot_password.blade.php
@@ -291,7 +291,7 @@
         </div>
 
         <div class="panel-right">
-            <a href="/login" class="back-to-login">
+            <a href="{{ route('login') }}" class="back-to-login">
                 <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"
                     stroke-linecap="round" stroke-linejoin="round">
                     <line x1="19" y1="12" x2="5" y2="12"></line>
